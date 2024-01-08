790913c5eef2334411a941570c8a30096df2effc
fatmamerve <mervesubasisarioglu@gmail.com>
2024-01-08 16:29:38 +0000

    v1

---
+++ src/test/java/da01_driverOlusturma/C02_IlkTest.java
@@ -0,0 +1,16 @@
+package da01_driverOlusturma;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_IlkTest {
+
+    public static void main(String[] args) {
+
+        System.setProperty("Webdriver.chrome.driver","src/kurulumDosyalari/chromedriver-mac-arm64/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.testotomasyonu.com");
+    }
+}
